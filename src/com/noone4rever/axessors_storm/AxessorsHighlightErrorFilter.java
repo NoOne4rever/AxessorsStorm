0ea2c998fed6e92ab3f742a4eb630f51d6c7157b
Maxim Abramov <32748406+NoOne4rever@users.noreply.github.com>
2018-01-16 18:51:38 +0300

    Generally improved performance.
    Added additional checkout for error severity and type. This change removes probable bug with unusual field highlighting.

--- src/com/noone4rever/axessors_storm/AxessorsHighlightErrorFilter.java
+++ src/com/noone4rever/axessors_storm/AxessorsHighlightErrorFilter.java
@@ -2,6 +2,7 @@ package com.noone4rever.axessors_storm;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoFilter;
+import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -17,22 +18,38 @@ public class AxessorsHighlightErrorFilter implements HighlightInfoFilter {
 
     private final static String METHOD_NOT_FOUND_PATTERN = "Method '%s.*' not found in.*";
     private final static String[] ACTIONS = {"get", "set", "add", "delete", "count", "increment", "decrement"};
-    private final static Pattern AXESSORS_COMMENT = Pattern.compile("#:.+");
+    private final static Pattern UNUSED_FIELD_PATTERN = Pattern.compile("Unused private field \\$.+");
+    private final static Pattern AXESSORS_COMMENT_PATTERN = Pattern.compile("#:.+");
 
+    /**
+     * Accepts or rejects IDE warnings.
+     *
+     * @param highlightInfo error description
+     * @param file PSI file
+     * @return the result of the error checkout
+     */
     @Override
+    @SuppressWarnings("SimplifiableIfStatement")
     public boolean accept(@NotNull HighlightInfo highlightInfo, @Nullable PsiFile file) {
-        String description = StringUtil.notNullize(highlightInfo.getDescription());
-        return !isAxessorsAction(description) && !isAxessorsField(highlightInfo, file);
+        if (file == null) {
+            return true;
+        }
+        HighlightSeverity severity = highlightInfo.getSeverity();
+        if (severity.equals(HighlightSeverity.INFORMATION)) {
+            return true;
+        }
+        return !isAxessorsAction(highlightInfo) && !isAxessorsField(highlightInfo, file);
     }
 
     /**
-     * Checks if an error is caused by Axessors method invocation.
+     * Checks if the error is caused by Axessors method invocation.
      *
-     * @param description error description
+     * @param highlightInfo error description
      * @return the result of the checkout
      */
-    private boolean isAxessorsAction(String description) {
+    private boolean isAxessorsAction(@NotNull HighlightInfo highlightInfo) {
         Pattern pattern;
+        String description = StringUtil.notNullize(highlightInfo.getDescription());
         for (String action : ACTIONS) {
             pattern = Pattern.compile(String.format(METHOD_NOT_FOUND_PATTERN, action));
             if (pattern.matcher(description).matches()) {
@@ -49,13 +66,18 @@ public class AxessorsHighlightErrorFilter implements HighlightInfoFilter {
      * @param file PSI file
      * @return the result of the checkout
      */
-    private boolean isAxessorsField(HighlightInfo highlightInfo, PsiFile file) {
+    @SuppressWarnings("ConstantConditions")
+    private boolean isAxessorsField(@NotNull HighlightInfo highlightInfo, @NotNull PsiFile file) {
+        String description = StringUtil.notNullize(highlightInfo.getDescription());
+        if (!UNUSED_FIELD_PATTERN.matcher(description).matches()) {
+            return false;
+        }
         try {
-            PsiElement field = file.findElementAt(highlightInfo.startOffset);
-            PsiElement nextElement = field.getParent().getParent().getNextSibling().getNextSibling();
+            PsiElement variable = file.findElementAt(highlightInfo.startOffset);
+            PsiElement nextElement = variable.getParent().getParent().getNextSibling().getNextSibling();
             String content = nextElement.getText();
-            return AXESSORS_COMMENT.matcher(content).matches();
-        } catch (NullPointerException e) {
+            return AXESSORS_COMMENT_PATTERN.matcher(content).matches();
+        } catch (NullPointerException error) {
             return false;
         }
     }
